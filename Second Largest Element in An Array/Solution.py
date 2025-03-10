9a3619ad1a64ca486fab4579351271e6359cac09
aishwary-03 <aishwary987654321@gmail.com>
2025-03-10 11:10:54 +0530

    Find Second Largest Element in An Array

---
+++ Second Largest Element in An Array/Solution.py
@@ -0,0 +1,17 @@
+from sys import *
+from collections import *
+from math import *
+
+def getSecondOrderElements(n: int,  a: [int]) -> [int]:
+    max = secondMax = a[0];
+    for ele in a:
+        if ele > max:
+            secondMax = max;
+            max = ele;
+        elif ele > secondMax and secondMax != max:
+            secondMax = ele;
+    return secondMax;
+
+secondLargestElement = getSecondOrderElements(5, [1,2,3,4,5,6,7,8]);
+
+print(secondLargestElement);
