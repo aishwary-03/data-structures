57a07333bf3e876c9688017c27a2475a0f84df1a
aishwary-03 <aishwary987654321@gmail.com>
2025-03-19 10:04:06 +0530

    Find the number that appears once, and the other numbers twice

---
+++ Find the number that appears once, and the other numbers twice/Solution.py
@@ -0,0 +1,9 @@
+def getSingleElement(arr : list[int]) -> int:
+    i = 0
+    while i < len(arr) - 1:
+        if arr[i] != arr[i + 1]:
+            return arr[i]
+        i = i + 2
+    return arr[-1]    
+
+print(getSingleElement([1, 1, 2, 3, 3, 4, 4]))
